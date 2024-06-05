0e57c571cc852b3e42965f8ebcb9c599cdfef9d3
Daniel Hoeflehner <daniel.hoeflehner@outlook.com>
2024-06-05 21:00:50 +0200

    Use hal with naive delay

--- src/main.rs
+++ src/main.rs
@@ -3,57 +3,41 @@
 
 extern crate panic_halt;
 
-use core::{
-    arch::asm,
-    ptr::{self},
-};
+use core::arch::asm;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{self, hprintln};
-
-const RCC: u32 = 0x4002_1000;
-const RCC_CR: *mut u32 = (RCC + 0x00) as *mut u32;
-const RCC_AHBENR: *mut u32 = (RCC + 0x14) as *mut u32;
-
-const GPIOE: u32 = 0x4800_1000;
-const GPIOE_MODER: *mut u32 = (GPIOE + 0x00) as *mut u32;
-const GPIOE_ODR: *mut u32 = (GPIOE + 0x14) as *mut u32;
-const GPIOE_BSRR: *mut u32 = (GPIOE + 0x18) as *mut u32;
+use stm32f3::stm32f303::{self};
 
 #[entry]
 fn main() -> ! {
     // You should see that in your openocd output
     hprintln!("Hello from Discovery");
 
-    unsafe {
-        let mut val = ptr::read_volatile(RCC_CR);
-        // Use the HSI as system clock (8MHz)
-        val |= 1 << 1;
-        ptr::write_volatile(RCC_CR, val);
+    let peripherals = stm32f303::Peripherals::take().unwrap();
 
-        val = ptr::read_volatile(RCC_AHBENR);
-        // Enable GPIO E clock
-        val |= 1 << 21;
-        ptr::write_volatile(RCC_AHBENR, val);
+    let rcc = &peripherals.RCC;
+    // Set HSI clock on
+    rcc.cr.write(|w| w.hsion().set_bit());
+    // Enable GPIO Port E clock
+    rcc.ahbenr.write(|w| w.iopeen().set_bit());
 
-        // Set Pin 9 to output: PE 9 = Bit 19 & 18
-        val = ptr::read_volatile(GPIOE_MODER);
-        val &= 0xFFF3_FFFF; // Clear Bit 19 & 18
-        val |= 1 << 18; // Set Bit 18
-        ptr::write_volatile(GPIOE_MODER, val);
+    let gpioe = &peripherals.GPIOE;
 
-        loop {
-            // Read in the output data register of GPIO Port E
-            let val = ptr::read_volatile(GPIOE_ODR);
-            // If Pin 9 is currently in High state, switch to Low, otherwise switch to High
-            if val & (1 << 9) == (1 << 9) {
-                // Set Pin 18 Low
-                ptr::write_volatile(GPIOE_BSRR, 1 << 25);
-            } else {
-                // Set Pin 18 High
-                ptr::write_volatile(GPIOE_BSRR, 1 << 9);
-            }
+    // Set Pin 9 to output
+    gpioe.moder.write(|w| w.moder9().output());
 
+    loop {
+        // Read Port E Pin 9 output data register
+        if gpioe.odr.read().odr9().bit_is_set() {
+            // Write to bit reset register to set Pin 9 Low
+            gpioe.bsrr.write(|w| w.br9().set_bit());
+        } else {
+            // Write to bit set register to set Pin 9 High
+            gpioe.bsrr.write(|w| w.bs9().set_bit());
+        }
+
+        unsafe {
             delay();
         }
     }
